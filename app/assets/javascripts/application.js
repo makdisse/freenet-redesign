7c895ca979cc9d27c44c7c869da7a8d5f3b20220
Paulo Makdisse <makdisse@gmail.com>
2013-07-03 16:01:15 +0200

    Done config interfaces page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,49 +15,66 @@
 //= require bootstrap
 
 //= require_tree .
-
+/*
+IF YOU ARE READING THIS YOU ARE WRONG!
+DID YOU READ THAT THE NAME OF THIS PAGE WAS 'MOCKUP'?
+THAT'S A PROTOTYPE
+*/
 $(document).ready(function() {
 
   //Setup the x-edit plugin
   $.fn.editable.defaults.mode = 'popup';
   $('.friend-note').editable();
 
-  //
-    $("[data-toggle='tooltip']").tooltip({
-        placement: 'right',
-        container: 'body',
-    });
+  //Setup all tooltip itens
+  $("[data-toggle='tooltip']").tooltip({
+    placement: 'right',
+    container: 'body',
+  });
 
-    $("[data-toggle='popover']").popover({
-        container: 'body',
-        html: 'true'
-    }).click(function(e) {
-        e.preventDefault();
-     });
+  //Setup all popover components
+  $("[data-toggle='popover']").popover({
+    container: 'body',
+    html: 'true'
+  }).click(function(e) {
+    e.preventDefault();
+  });
 
-  $("#simpleModeBtn").button('toggle');
-
-  //if have a hash open the modal
+  //if have a hash in the url open the modal
   if (window.location.hash) {
     $(window.location.hash).modal('show');
   }
 
-//Simulate buttons usability
-  $("#enable_buttons").change(function()  {
-    if ($("#enable_buttons").prop("checked") ==  true) {
-      $("#btn_n2ntm").attr('disabled',false);
-      $("#btn_trust").attr('disabled',false);
-      $("#btn_visibility").attr('disabled',false);
-      $("#btn_remove").attr('disabled',false);
-    } else {
-      $("#btn_n2ntm").attr('disabled',true);
-      $("#btn_trust").attr('disabled',true);
-      $("#btn_visibility").attr('disabled',true);
-      $("#btn_remove").attr('disabled',true);
-    }
+//Simulate buttons usability in the friends connections page
+$("#enable_buttons").change(function()  {
+  if ($("#enable_buttons").prop("checked") ==  true) {
+    $("#btn_n2ntm").attr('disabled',false);
+    $("#btn_trust").attr('disabled',false);
+    $("#btn_visibility").attr('disabled',false);
+    $("#btn_remove").attr('disabled',false);
+  } else {
+    $("#btn_n2ntm").attr('disabled',true);
+    $("#btn_trust").attr('disabled',true);
+    $("#btn_visibility").attr('disabled',true);
+    $("#btn_remove").attr('disabled',true);
+  }
+})
+
+//Always toggle simpleModeBtn
+$(".simpleModeBtn").button('toggle');
+
+//Simple and advanced mode
+$(".simpleModeBtn").click(function() {
+  $(".advanced[data-parent='" + $(this).data("parenttohide") + "']").slideUp();
+})
+$(".advancedModeBtn").click(function() {
+  $(".advanced[data-parent='" + $(this).data("parenttohide") + "']").show("slow");
+})
 
 
-  })
+
+
+
 
 })
 
